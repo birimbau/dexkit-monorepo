6c0309f43c03f49b6d447ac30cad782acf569b89
João Campos <jcampos893@gmail.com>
2023-03-20 12:17:27 +0000

    feat/add filters kits to admin (#41)
    * feat: add builder kits filters

---
+++ apps/nft-marketplace-premium/src/modules/wizard/components/BuilderKitMenu.tsx
@@ -0,0 +1,69 @@
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import * as React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { BuilderKit } from '../constants';
+
+interface Props {
+  menu: BuilderKit;
+  onChangeMenu: (newMenu: BuilderKit) => void;
+}
+
+export default function BuilderKitMenu(props: Props) {
+  const { menu, onChangeMenu } = props;
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <Button
+        id="builderkit-menu"
+        aria-controls={open ? 'builderkit-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        sx={{
+          fontWeight: 600,
+          textDecoration: 'none',
+          color: 'text.primary',
+          textTransform: 'none',
+          fontSize: 'inherit',
+        }}
+        endIcon={<ExpandMoreIcon />}
+        onClick={handleClick}
+      >
+        <FormattedMessage id={menu.toLowerCase()} defaultMessage={menu} />
+      </Button>
+      <Menu
+        id="builderkit-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'builderkit-menu',
+        }}
+      >
+        {Object.values(BuilderKit).map((m, k) => (
+          <MenuItem
+            onClick={() => {
+              console.log(m);
+              onChangeMenu(m);
+              handleClose();
+            }}
+            key={k}
+          >
+            <FormattedMessage id={m.toLowerCase()} defaultMessage={m} />
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
+  );
+}
