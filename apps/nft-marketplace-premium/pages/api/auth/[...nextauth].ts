d1d79d6229822537d3d81ca83c6386b5ba00da01
JoaoCampos89 <jcampos893@gmail.com>
2023-03-21 10:08:38 -0300

    feat: add discord

--- apps/nft-marketplace-premium/pages/api/auth/[...nextauth].ts
+++ apps/nft-marketplace-premium/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,20 @@
-import NextAuth from "next-auth"
+import NextAuth, { AuthOptions } from "next-auth"
+import DiscordProvider from "next-auth/providers/discord"
 import TwitterProvider from "next-auth/providers/twitter"
-export const authOptions = {
+export const authOptions: AuthOptions = {
   // Configure one or more authentication providers
   providers: [
     TwitterProvider({
       clientId: process.env.TWITTER_API_KEY || '',
       clientSecret: process.env.TWITTER_API_KEY_SECRET || ''
     }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID || '',
+      clientSecret: process.env.DISCORD_CLIENT_SECRET || ''
+    })
     // ...add more providers here
   ],
+
+
 }
 export default NextAuth(authOptions)
