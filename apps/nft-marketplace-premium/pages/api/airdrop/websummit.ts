d6024e2ed288607329a10f496537def4c6014fa3
JoaoCampos89 <jcampos893@gmail.com>
2023-04-28 09:30:01 -0300

    fix:encode string

--- apps/nft-marketplace-premium/pages/api/airdrop/websummit.ts
+++ apps/nft-marketplace-premium/pages/api/airdrop/websummit.ts
@@ -14,7 +14,7 @@ export default async function handler(
   }
   console.log(req.headers['x-vercel-ip-city']);
   console.log(req.headers['x-vercel-ip-country-region'])
-  if (req.headers['x-vercel-ip-country'] === 'BR' && req.headers['x-vercel-ip-city'] === 'Rio de Janeiro') {
+  if (req.headers['x-vercel-ip-country'] === 'BR' && req.headers['x-vercel-ip-city'] === encodeURIComponent('Rio de Janeiro')) {
 
   } else {
     return res.status(401).json({ message: `You not attend requirements for airdrop: ${req.headers['x-vercel-ip-country']}: ${req.headers['x-vercel-ip-city']}` });
