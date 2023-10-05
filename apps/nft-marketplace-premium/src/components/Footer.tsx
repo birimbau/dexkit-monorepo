9a923e525121796a07b7532aafc4ca3b85bed9d5
JoaoCampos89 <jcampos893@gmail.com>
2023-10-05 17:37:37 -0300

    fix: footer sometimes white

--- apps/nft-marketplace-premium/src/components/Footer.tsx
+++ apps/nft-marketplace-premium/src/components/Footer.tsx
@@ -73,7 +73,7 @@ export function Footer({ appConfig, isPreview, appNFT }: Props) {
     <Box
       py={2}
       sx={{
-        bgcolor: (theme) => theme.palette.background.paper,
+        bgcolor: 'background.paper',
         minHeight: '50px',
         width: '100%',
       }}
@@ -113,7 +113,7 @@ export function Footer({ appConfig, isPreview, appNFT }: Props) {
                         defaultMessage={m.name}
                       />
                     </Link>
-                  )
+                  ),
                 )}
               </Stack>
             ) : (
