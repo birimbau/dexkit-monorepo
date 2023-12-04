042308c90efc9e621cc1944b999de6b83e0ddee3
JoaoCampos89 <jcampos893@gmail.com>
2023-12-04 08:48:25 -0300

    fix: exchange networks

--- packages/dexkit-exchange/constants/index.ts
+++ packages/dexkit-exchange/constants/index.ts
@@ -5,25 +5,26 @@ export const ORDER_LIMIT_DURATIONS: {
   defaultMessage: string;
   value: number;
 }[] = [
-  { messageId: "five.minutes", defaultMessage: "5 minutes", value: 5 * 60 },
-  { messageId: "ten.minutes", defaultMessage: "10 minutes", value: 10 * 60 },
-  { messageId: "one.hour", defaultMessage: "1 hour", value: 60 * 60 },
-  { messageId: "two.hours", defaultMessage: "2 hours", value: 2 * 60 * 60 },
-  {
-    messageId: "twenty.four.hours",
-    defaultMessage: "24 hours",
-    value: 24 * 60 * 60,
-  },
-];
+    { messageId: "five.minutes", defaultMessage: "5 minutes", value: 5 * 60 },
+    { messageId: "ten.minutes", defaultMessage: "10 minutes", value: 10 * 60 },
+    { messageId: "one.hour", defaultMessage: "1 hour", value: 60 * 60 },
+    { messageId: "two.hours", defaultMessage: "2 hours", value: 2 * 60 * 60 },
+    {
+      messageId: "twenty.four.hours",
+      defaultMessage: "24 hours",
+      value: 24 * 60 * 60,
+    },
+  ];
 
 export const GECKOTERMINAL_NETWORK: { [key: number]: string } = {
   [ChainId.Ethereum]: "eth",
   [ChainId.Polygon]: "polygon_pos",
   [ChainId.Arbitrum]: "arbitrum",
   [ChainId.BSC]: "bsc",
-  [ChainId.Avax]: "abax",
+  [ChainId.Avax]: "avax",
   [ChainId.Celo]: "celo",
   [ChainId.Fantom]: "ftm",
+  [ChainId.Optimism]: "optimism",
   [ChainId.Base]: "base",
 };
 
