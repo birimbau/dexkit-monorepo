96f55d4417fce41fd2ed6d738a6939b263636b64
aecdanjundex <85881131+aecdanjundex@users.noreply.github.com>
2023-12-11 08:56:27 -0300

    fix: bugs on airdrop contracts and make adjustments (#182)

---
+++ apps/nft-marketplace-premium/src/modules/contract-wizard/components/containers/ContractAirdropErc1155BalanceDialog.tsx
@@ -0,0 +1,59 @@
+import { AppDialogTitle } from '@dexkit/ui';
+import {
+  Dialog,
+  DialogContent,
+  DialogProps,
+  Divider,
+  List,
+} from '@mui/material';
+import { useContract, useNFTs } from '@thirdweb-dev/react';
+import { useWeb3React } from '@web3-react/core';
+import { FormattedMessage } from 'react-intl';
+import ContractAirdropErc1155ListItem from './ContractAirdropErc1155ListItem';
+
+export interface CotnractAirdrop1155BalanceDialogProps {
+  DialogProps: DialogProps;
+  contractAddres?: string;
+}
+
+export default function ContractAirdrop1155BalanceDialog({
+  DialogProps,
+  contractAddres,
+}: CotnractAirdrop1155BalanceDialogProps) {
+  const { onClose } = DialogProps;
+
+  const handleClose = () => {
+    if (onClose) {
+      onClose({}, 'backdropClick');
+    }
+  };
+
+  const { data: contract } = useContract(contractAddres);
+  const { data: nfts } = useNFTs(contract);
+
+  const { account } = useWeb3React();
+
+  return (
+    <Dialog {...DialogProps}>
+      <AppDialogTitle
+        title={
+          <FormattedMessage id="your.tokens" defaultMessage="Your tokens" />
+        }
+        onClose={handleClose}
+      />
+      <Divider />
+      <DialogContent sx={{ p: 0 }}>
+        <List disablePadding>
+          {nfts?.map((nft, key) => (
+            <ContractAirdropErc1155ListItem
+              key={key}
+              contractAddress={contractAddres}
+              tokenId={nft.metadata.id}
+              account={account}
+            />
+          ))}
+        </List>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- packages/dexkit-ui/components/dialogs/AppDataTableDialog.tsx
+++ packages/dexkit-ui/components/dialogs/AppDataTableDialog.tsx
@@ -1,12 +1,15 @@
 import AppDataTable from "@dexkit/ui/components/AppDataTable";
 import {
+  Alert,
+  Box,
   Button,
   Dialog,
   DialogActions,
   DialogContent,
   DialogProps,
 } from "@mui/material";
-import React, { useState } from "react";
+import { GridRowId } from "@mui/x-data-grid";
+import React, { useMemo, useState } from "react";
 import { FormattedMessage } from "react-intl";
 import { AppDialogTitle } from "../AppDialogTitle";
 
@@ -32,11 +35,14 @@ export default function AppDataTableDialog({
 }: AppDataTableDialogProps) {
   const { onClose } = DialogProps;
 
+  const [editRows, setEditRow] = useState<{ [key: GridRowId]: boolean }>({});
+
   const handleClose = () => {
     if (onClose) {
       onClose({}, "backdropClick");
     }
     setValues(undefined);
+    setEditRow({});
   };
 
   const [values, setValues] =
@@ -53,6 +59,19 @@ export default function AppDataTableDialog({
     setValues(data);
   };
 
+  const handleEditRow = (id: GridRowId, value: boolean) => {
+    setEditRow((values) => ({ ...values, [id]: value }));
+  };
+
+  const canSave = useMemo(() => {
+    const rowValues = Object.values(editRows);
+    return rowValues.findIndex((c) => c) === -1;
+  }, [editRows]);
+
+  const isEmpty = useMemo(() => {
+    return values && values?.length === 0;
+  }, [values]);
+
   return (
     <Dialog {...DialogProps}>
       <AppDialogTitle title={title} />
@@ -61,10 +80,34 @@ export default function AppDataTableDialog({
           dataColumns={dataColumns}
           data={value}
           onChange={handleChange}
+          onEditRow={handleEditRow}
         />
+
+        <Box p={2}>
+          {isEmpty && (
+            <Alert severity="warning">
+              <FormattedMessage
+                id="add.addresses.to.airdrop"
+                defaultMessage="Add addresses to airdrop"
+              />
+            </Alert>
+          )}
+          {!canSave && (
+            <Alert severity="error">
+              <FormattedMessage
+                id="save.your.row.changes.before.confirm"
+                defaultMessage="Save your row changes before confirm"
+              />
+            </Alert>
+          )}
+        </Box>
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleConfirm} variant="contained">
+        <Button
+          disabled={isEmpty || !canSave}
+          onClick={handleConfirm}
+          variant="contained"
+        >
           <FormattedMessage id="confirm" defaultMessage="Confirm" />
         </Button>
         <Button onClick={handleClose}>
