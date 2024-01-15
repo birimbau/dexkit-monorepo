dff77e81ba0870888e6ea5c81607573af881c921
JoaoCampos89 <jcampos893@gmail.com>
2024-01-15 18:43:02 -0300

    chore: fix ann

--- apps/nft-marketplace-premium/src/components/AppBarANN.tsx
+++ apps/nft-marketplace-premium/src/components/AppBarANN.tsx
@@ -11,43 +11,45 @@ export function AppBarANN() {
   }
 
   return (
-    (hostname === 'dexappbuilder.dexkit.com' ||
-      hostname === 'dexappbuilder.com') && (
-      <AppBar position="static" sx={{ pt: 1, pb: 1 }}>
-        <Box display={'flex'} justifyContent={'center'}>
-          <Typography sx={{ pr: 1 }}>😉👉</Typography>
-          <Typography>
-            <FormattedMessage
-              id={'dexappbuilder.ann.app.bar'}
-              defaultMessage={
-                'Support us on {Giveth} and allow our service to be free for all. {Gitcoin} score 4 and donation on Polygon to us have matching donations!'
-              }
-              values={{
-                Giveth: (
-                  <Link
-                    color={'secondary'}
-                    href={
-                      'https://giveth.io/project/dexappbuilder-the-no-codelow-code-toolkit-of-dexkit'
-                    }
-                    target="_blank"
-                  >
-                    Giveth
-                  </Link>
-                ),
-                Gitcoin: (
-                  <Link
-                    color={'secondary'}
-                    href={'https://passport.gitcoin.co/'}
-                    target="_blank"
-                  >
-                    Gitcoin Passport
-                  </Link>
-                ),
-              }}
-            />
-          </Typography>
-        </Box>
-      </AppBar>
-    )
+    <>
+      {(hostname === 'dexappbuilder.dexkit.com' ||
+        hostname === 'dexappbuilder.com') && (
+        <AppBar position="static" sx={{ pt: 1, pb: 1 }}>
+          <Box display={'flex'} justifyContent={'center'}>
+            <Typography sx={{ pr: 1 }}>😉👉</Typography>
+            <Typography>
+              <FormattedMessage
+                id={'dexappbuilder.ann.app.bar'}
+                defaultMessage={
+                  'Support us on {Giveth} and allow our service to be free for all. {Gitcoin} score 4 and donation on Polygon to us have matching donations!'
+                }
+                values={{
+                  Giveth: (
+                    <Link
+                      color={'secondary'}
+                      href={
+                        'https://giveth.io/project/dexappbuilder-the-no-codelow-code-toolkit-of-dexkit'
+                      }
+                      target="_blank"
+                    >
+                      Giveth
+                    </Link>
+                  ),
+                  Gitcoin: (
+                    <Link
+                      color={'secondary'}
+                      href={'https://passport.gitcoin.co/'}
+                      target="_blank"
+                    >
+                      Gitcoin Passport
+                    </Link>
+                  ),
+                }}
+              />
+            </Typography>
+          </Box>
+        </AppBar>
+      )}
+    </>
   );
 }
