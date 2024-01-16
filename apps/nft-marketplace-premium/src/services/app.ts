df1a89748f58f893dbcb4abd9e86344db51a5159
JoaoCampos89 <jcampos893@gmail.com>
2024-01-15 23:03:39 -0300

    chore

--- apps/nft-marketplace-premium/src/services/app.ts
+++ apps/nft-marketplace-premium/src/services/app.ts
@@ -83,8 +83,8 @@ export async function getAppConfig(
 
 
   if (site?.startsWith('localhost')) {
-    //const [slug,] = site?.split('.') || [];
-    const slug = 'arbitrum';
+    const [slug,] = site?.split('.') || [];
+    //const slug = 'arbitrum';
     if (slug) {
       const configResponse = (await getConfig({ slug, appPage })).data;
 
