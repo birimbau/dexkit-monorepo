c07ac0af14dfc925f74f5066b23b95869115dfba
JoaoCampos89 <jcampos893@gmail.com>
2024-02-07 17:02:46 -0300

    feat: add lightbox on asset image

--- apps/nft-marketplace-premium/src/modules/nft/components/AssetImage.tsx
+++ apps/nft-marketplace-premium/src/modules/nft/components/AssetImage.tsx
@@ -1,8 +1,11 @@
 import { Box } from '@mui/material';
+import dynamic from 'next/dynamic';
 import Image from 'next/future/image';
+import { useState } from 'react';
 import { useIntl } from 'react-intl';
 import { isWhitelistedDomain } from '../../../utils/image';
 import { ipfsUriToUrl } from '../../../utils/ipfs';
+const LightBoxDialog = dynamic(() => import('src/components/LightBoxDialog'));
 
 interface Props {
   src: string;
@@ -10,45 +13,59 @@ interface Props {
 
 export function AssetImage({ src }: Props) {
   const { formatMessage } = useIntl();
+  const [openLightBox, setOpenLightBox] = useState(false);
 
   return (
-    <Box
-      sx={{
-        position: 'relative',
-        width: '100%',
-        paddingTop: '100%',
-      }}
-    >
-      {isWhitelistedDomain(src) ? (
-        <Image
-          src={ipfsUriToUrl(src)}
-          style={{
-            position: 'absolute',
-            top: 0,
-            width: '100%',
+    <>
+      {openLightBox && (
+        <LightBoxDialog
+          imageUrl={ipfsUriToUrl(src)}
+          open={openLightBox}
+          onClose={() => {
+            setOpenLightBox(false);
           }}
-          fill
-          alt={formatMessage({
-            id: 'nft.image',
-            defaultMessage: 'NFT Image',
-          })}
-        />
-      ) : (
-        <Image
-          src={ipfsUriToUrl(src)}
-          unoptimized={true}
-          style={{
-            position: 'absolute',
-            top: 0,
-            width: '100%',
-          }}
-          fill
-          alt={formatMessage({
-            id: 'nft.image',
-            defaultMessage: 'NFT Image',
-          })}
         />
       )}
-    </Box>
+      <Box
+        onClick={() => setOpenLightBox(true)}
+        sx={{
+          position: 'relative',
+          width: '100%',
+          paddingTop: '100%',
+          cursor: 'pointer',
+        }}
+      >
+        {isWhitelistedDomain(src) ? (
+          <Image
+            src={ipfsUriToUrl(src)}
+            style={{
+              position: 'absolute',
+              top: 0,
+              width: '100%',
+            }}
+            fill
+            alt={formatMessage({
+              id: 'nft.image',
+              defaultMessage: 'NFT Image',
+            })}
+          />
+        ) : (
+          <Image
+            src={ipfsUriToUrl(src)}
+            unoptimized={true}
+            style={{
+              position: 'absolute',
+              top: 0,
+              width: '100%',
+            }}
+            fill
+            alt={formatMessage({
+              id: 'nft.image',
+              defaultMessage: 'NFT Image',
+            })}
+          />
+        )}
+      </Box>
+    </>
   );
 }
