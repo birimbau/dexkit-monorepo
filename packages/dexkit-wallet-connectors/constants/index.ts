633234c1fcdddfa3d5f82bb23019f430f75e5340
JoaoCampos89 <jcampos893@gmail.com>
2024-03-07 18:32:33 -0300

    feat: finish details

--- packages/dexkit-wallet-connectors/constants/index.ts
+++ packages/dexkit-wallet-connectors/constants/index.ts
@@ -2,11 +2,11 @@ import { Web3ReactHooks } from "@web3-react/core";
 import { Connector } from "@web3-react/types";
 
 
-import { coinbaseWalletConnection, deprecatedInjectedConnection, magic, magicHooks, walletConnectV2Connection } from "../connectors/connections";
+import { deprecatedInjectedConnection, magic, magicHooks, walletConnectV2Connection } from "../connectors/connections";
 
 export const CONNECTORS: { [key: string]: [Connector, Web3ReactHooks] } = {
   metamask: [deprecatedInjectedConnection.connector, deprecatedInjectedConnection.hooks],
   walletConnect: [walletConnectV2Connection.connector, walletConnectV2Connection.hooks],
   magic: [magic, magicHooks],
-  coinbase: [coinbaseWalletConnection.connector, coinbaseWalletConnection.hooks]
+  // coinbase: [coinbaseWalletConnection.connector, coinbaseWalletConnection.hooks]
 };
