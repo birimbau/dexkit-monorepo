d3776145a2891ba15ea8a143cfbfb83128e51ebf
JoaoCampos89 <jcampos893@gmail.com>
2024-04-08 21:06:57 -0300

    chore: remove ANN

--- apps/nft-marketplace-premium/pages/_app.tsx
+++ apps/nft-marketplace-premium/pages/_app.tsx
@@ -273,7 +273,7 @@ export default function MyApp(props: MyAppProps) {
                       >
                         <CircularProgress color="inherit" size={80} />
                       </Backdrop>
-                      <AppBarANN />
+                      {false && <AppBarANN />}
                       {getLayout(<Component {...pageProps} />)}
                     </AppMarketplaceProvider>
                   </LocalizationProvider>
