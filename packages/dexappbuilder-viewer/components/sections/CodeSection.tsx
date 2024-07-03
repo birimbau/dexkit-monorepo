5274ef68c4a912fe07355606280fb104dc74f203
Aécio Júnior <aecdanjundex@gmail.com>
2024-07-03 11:18:09 -0300

    feat: remove unused code

--- packages/dexappbuilder-viewer/components/sections/CodeSection.tsx
+++ packages/dexappbuilder-viewer/components/sections/CodeSection.tsx
@@ -10,54 +10,25 @@ export default function CodeSection({ section }: CodeSectionProps) {
 
   const divRef = useRef<HTMLDivElement | null>(null);
 
-  const invalidRef = useRef(false);
-
-  /* useEffect(() => {
-    const script = document.createElement('script');
-
-    script.innerHTML = js;
-    script.async = true;
-
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, [js]);*/
-
   useEffect(() => {
     if (divRef.current) {
-      try {
-        divRef.current.innerHTML = "";
+      divRef.current.innerHTML = "";
 
-        const range = document.createRange();
-        const rangeCss = document.createRange();
-        const rangeJs = document.createRange();
+      const range = document.createRange();
+      const rangeCss = document.createRange();
+      const rangeJs = document.createRange();
 
-        const cssFrag = rangeCss.createContextualFragment(
-          `<style>${css}</style>`
-        );
-        const htmlFrag = range.createContextualFragment(html);
-        const jsFrag = rangeJs.createContextualFragment(
-          `<script>${js}</script>`
-        );
+      const cssFrag = rangeCss.createContextualFragment(
+        `<style>${css}</style>`
+      );
+      const htmlFrag = range.createContextualFragment(html);
+      const jsFrag = rangeJs.createContextualFragment(`<script>${js}</script>`);
 
-        divRef.current.appendChild(cssFrag);
-        divRef.current.appendChild(htmlFrag);
-        divRef.current.appendChild(jsFrag);
-
-        invalidRef.current = false;
-      } catch (err) {
-        invalidRef.current = true;
-      }
+      divRef.current.appendChild(cssFrag);
+      divRef.current.appendChild(htmlFrag);
+      divRef.current.appendChild(jsFrag);
     }
   }, [divRef.current, html, css, js]);
 
-  return (
-    <>
-      {/* {css && <Widget htmlString={`<style>${css}</style>`} />}
-      {js && <Widget htmlString={`<script>${js}<script>`} />} */}
-      {!invalidRef.current && <div ref={divRef}></div>}
-    </>
-  );
+  return <div ref={divRef}></div>;
 }
